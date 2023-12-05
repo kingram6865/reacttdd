7000e7f9587536361428db283e499a5c72497e09
Ken Ingram <ken.ingram@gmail.com>
2023-12-04 20:30:16 -0800

    Add testt for hiding form before and after use.

--- cypress/e2e/adding_a_restaurant.spec.cy.js
+++ cypress/e2e/adding_a_restaurant.spec.cy.js
@@ -3,9 +3,11 @@ describe('adding a restaurant', () => {
     const restaurantName = 'Sushi Place'
 
     cy.visit('http://localhost:1234')
+    cy.get('[data-testid=newRestaurantName]').should('not.exist') 
     cy.get('[data-testid=addRestaurantButton]').click();
     cy.get('[data-testid=newRestaurantName]').type(restaurantName)
     cy.get('[data-testid=saveNewRestaurantButton]').click()
+    cy.get('[data-testid=newRestaurantName]').should('not.exist') 
 
     cy.contains(restaurantName)
   })
