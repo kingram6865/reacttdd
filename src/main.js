792dd80a81fe894cf4de40317338cfb071b96671
Ken Ingram <ken.ingram@gmail.com>
2023-12-04 18:35:37 -0800

    ReactDOM.render is no longer supported in React 18.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = document.getElementById('root');
-
-ReactDOM.render(
-  <App />,
-  root,
-);
-
+const root = createRoot(document.getElementById('root'));
+root.render(<App />)
